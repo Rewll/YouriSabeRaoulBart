326e28173f963521299676828d6300b80056d22a
Raoul Scholtens <Raoulscholtens@gmail.com>
2022-04-26 15:34:56 +0200

    scriptable objecten

---
+++ crafting de pafting poging twee/Assets/Scripts/SeksLatrine.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SeksLatrine : MonoBehaviour
+{
+    public Test testingSERKS;   
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(testingSERKS.roekoe[1]);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ crafting de pafting poging twee/Assets/Scripts/Test.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "sjoepiedoe", menuName = "ScriptableObjects/Sjoepdieo")]
+
+public class Test : ScriptableObject
+{
+    public int raoul = 3;
+    public List<int> roekoe = new List<int>();
+}
