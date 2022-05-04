0f6756f6e43b5109cdaa86f815cf219ba5403b7c
Poesbroek <78036828+Poesbroek@users.noreply.github.com>
2022-05-04 03:28:41 +0200

    Moving bot has been in the files 👀

---
+++ crafting de pafting poging twee/Assets/Youri Map/Script/AirEffect.cs
@@ -0,0 +1,16 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AirEffect : IMainEffect
+{
+    public void DrinkEffect()
+    {
+        throw new System.NotImplementedException();
+    }
+
+    public void ThrowEffect()
+    {
+        throw new System.NotImplementedException();
+    }
+}
