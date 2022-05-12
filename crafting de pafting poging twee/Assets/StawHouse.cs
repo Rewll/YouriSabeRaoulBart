6bb733731c3444695640c21a162dca1fff6ffbba
Poesbroek <78036828+Poesbroek@users.noreply.github.com>
2022-05-12 20:09:35 +0200

    TagGebeuren

---
+++ crafting de pafting poging twee/Assets/StawHouse.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StawHouse : MonoBehaviour
+{
+    private Animator animator;
+
+    private void Awake()
+    {
+        animator = gameObject.GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        Debug.Log(collision.tag);
+        if (collision.CompareTag("WindAblility")) 
+        {
+            animator.Play("StrowKapot");
+            Debug.Log("omgewaait");
+        }
+    }
+}
