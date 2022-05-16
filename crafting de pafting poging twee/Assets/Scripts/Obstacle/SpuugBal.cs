6a6797454c279f28b03af7332af34a7f9dae4c7b
Poesbroek <78036828+Poesbroek@users.noreply.github.com>
2022-05-16 15:18:37 +0200

    Added Brandbaar on stroohuis and NightCheck on Worp

--- crafting de pafting poging twee/Assets/Scripts/Obstacle/SpuugBal.cs
+++ crafting de pafting poging twee/Assets/Scripts/Obstacle/SpuugBal.cs
@@ -23,7 +23,7 @@ public class SpuugBal : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Ingredient") || collision.CompareTag("Potion")) 
+        if (collision.CompareTag("Ingredient") || collision.CompareTag("Potion") || collision.CompareTag("Enemy")) 
         {
             return;
         }
