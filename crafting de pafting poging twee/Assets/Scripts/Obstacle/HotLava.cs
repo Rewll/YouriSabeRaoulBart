7a5ee4ba7950f7c59fec50880ba719db0d72415d
SabeCrane <sabekraan@gmail.com>
2022-06-07 21:32:34 +0200

    Lava Obstacle

---
+++ crafting de pafting poging twee/Assets/Scripts/Obstacle/HotLava.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HotLava : MonoBehaviour
+{
+    public GameObject player;
+    private SpelerVuur spelerVuur;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        spelerVuur = player.GetComponent<SpelerVuur>();
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (!spelerVuur)
+        {
+            collision.transform.position = new Vector2(0, 0);
+        }
+        else
+        {
+            return;
+        }
+    }
+}
