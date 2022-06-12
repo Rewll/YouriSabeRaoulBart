c9e8f80e08db3603c06fa1b2be896b9db02076c3
Raoul Scholtens <Raoulscholtens@gmail.com>
2022-06-12 15:02:01 +0200

    Ingredienten gooibaar gemaakt

--- crafting de pafting poging twee/Assets/Scripts/Potion/Ingredient.cs
+++ crafting de pafting poging twee/Assets/Scripts/Potion/Ingredient.cs
@@ -7,14 +7,24 @@ public class Ingredient : MonoBehaviour
     [SerializeField] private PotionIngredient potionIngredientSO;
     [SerializeField] private GameObject player;
     public IngredientGenerator ING;
+    private PlayerStatsSO playerStatSORef;
+
+    [SerializeField]
+    private bool hasBeenPickedUpByPlayer;
+    private bool wordtGegooid;
+
+    Vector3 mousePos;
+    public float MoveSpeed = 10;
 
     private void Awake()
     {
         player = GameObject.Find("Player");
+        playerStatSORef = Resources.Load<PlayerStatsSO>("ScriptableObjects/PlayerStats");
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
+        wordtGegooid = false;
         if (collision.gameObject.CompareTag("Cauldron"))
         {
             var Script = collision.gameObject.GetComponent<Cauldron>();
@@ -26,6 +36,44 @@ public class Ingredient : MonoBehaviour
             ING.instantieerIngredient();
             }
             Destroy(gameObject);
+        }
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if (!collision.CompareTag("Player"))
+        {
+            wordtGegooid = false;
+        }
+    }
+
+    public void Update()
+    {
+        if (transform.parent)
+        {
+            hasBeenPickedUpByPlayer = true;
+        }
+        else
+        {
+            hasBeenPickedUpByPlayer = false;
+        }
+
+        if (hasBeenPickedUpByPlayer && Input.GetMouseButtonDown(playerStatSORef.throwButton))
+        {
+            transform.parent = null;
+            wordtGegooid = true;
+            mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePos.z = 0;
+        }
+
+        if (wordtGegooid)
+        {
+            transform.position = Vector2.MoveTowards(transform.position, mousePos, MoveSpeed * Time.deltaTime);
+        }
+
+        if (transform.position == mousePos) 
+        {
+            wordtGegooid = false;
         }
     }
 }
