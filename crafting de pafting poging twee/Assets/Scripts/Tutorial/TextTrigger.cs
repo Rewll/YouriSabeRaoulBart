c0d347c3ca3d02e6b67f67c4c071d301de94fb99
SabeCrane <sabekraan@gmail.com>
2022-06-12 21:35:20 +0200

    Cousin_Start

--- crafting de pafting poging twee/Assets/Scripts/Tutorial/TextTrigger.cs
+++ crafting de pafting poging twee/Assets/Scripts/Tutorial/TextTrigger.cs
@@ -6,12 +6,21 @@ using UnityEngine.Events;
 public class TextTrigger : MonoBehaviour
 {
     public UnityEvent triggerEnter;
+    public UnityEvent triggerExit;
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.CompareTag("Player"))
         {
             triggerEnter.Invoke();
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            triggerExit.Invoke();
         }
     }
 }
