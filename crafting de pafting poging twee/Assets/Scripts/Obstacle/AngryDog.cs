e92ef5797f18c3d205e77021c07056b4b95b7973
SabeCrane <sabekraan@gmail.com>
2022-06-13 22:12:25 +0200

    Obstacles finished

--- crafting de pafting poging twee/Assets/Scripts/Obstacle/AngryDog.cs
+++ crafting de pafting poging twee/Assets/Scripts/Obstacle/AngryDog.cs
@@ -3,29 +3,41 @@ using UnityEngine;
 public class AngryDog : MonoBehaviour
 {
     private Rigidbody2D dogRigidbody;
-    public Transform playerTransform;
+    public GameObject playerGameObject;
+    //public Transform playerTransform;
     public Transform respawnPoint;
     public Transform dogHouseTransform;
+    public GameObject dogCasualFace;
+    private CloudEntrance cloudEntrance;
 
     public float movementSpeed;
 
     private void Start()
     {
         dogRigidbody = this.GetComponent<Rigidbody2D>();
+        cloudEntrance = playerGameObject.GetComponent<CloudEntrance>();
     }
 
     private void Update()
-    {
-        AttackPlayer();
+    {  
+        if(playerGameObject.layer != 8)
+        {
+            AttackPlayer();
+        }
+        else
+        {
+            ReturnToDogHouse();
+        }
     }
 
     void AttackPlayer()
     {
-        Vector2 moveDirection = playerTransform.position - this.transform.position;
+        Vector2 moveDirection = playerGameObject.transform.position - this.transform.position;
         moveDirection.Normalize();
-        if (playerTransform.position != respawnPoint.position && Vector2.Distance(transform.position, playerTransform.position) <= 10f)
+        if (playerGameObject.transform.position != respawnPoint.position && Vector2.Distance(transform.position, playerGameObject.transform.position) <= 8f)
         {
             dogRigidbody.MovePosition((Vector2)transform.position + (moveDirection * movementSpeed * Time.deltaTime));
+            dogCasualFace.SetActive(false);
         }
         else
         {
@@ -40,14 +52,21 @@ public class AngryDog : MonoBehaviour
         if (Vector2.Distance(transform.position, dogHouseTransform.position) > 2f)
         {
             dogRigidbody.MovePosition((Vector2)transform.position + (moveDirection * movementSpeed * Time.deltaTime));
+            dogCasualFace.SetActive(true);
         }
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (collision.CompareTag("Player") && playerGameObject.layer != 8)
         {
             collision.transform.position = respawnPoint.position;
+        }
+
+        if (collision.CompareTag("Pointy"))
+        {
+            Destroy(collision.gameObject);
+            ReturnToDogHouse();
         }
     }
 }
